6795d726cd14f96f5a7205121fe5a3056e1a1264
Gerald Then <gthen@mdsoft.com.do>
2023-08-02 07:53:45 -0400

    finish & build

--- src/app/countries/services/countries.service.ts
+++ src/app/countries/services/countries.service.ts
@@ -1,14 +1,43 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable, catchError, of, map } from 'rxjs';
+import { Observable, catchError, of, map, tap } from 'rxjs';
+
 import { Country } from '../interfaces/country';
+import { CacheStore } from '../interfaces/cache-store.interface';
+import { Region } from '../interfaces/region.type';
 
 @Injectable({providedIn: 'root'})
 export class CountriesService {
 
   private apiUrl:string = 'https://restcountries.com/v3.1'
 
-  constructor( private http: HttpClient ) { }
+  public cacheStore: CacheStore = {
+    byCapital:    { term: '', countries:[] },
+    byCountries:  { term: '', countries:[] },
+    byRegion:     { region: '', countries:[] },
+  }
+
+  constructor( private http: HttpClient ) {
+    this.loadFromLocalStorage();
+   }
+
+private saveToLocalStorage() {
+  localStorage.setItem('cacheStore', JSON.stringify( this.cacheStore ) )
+}
+
+private loadFromLocalStorage() {
+   if( !localStorage.getItem('cacheStore') ) return;
+
+   this.cacheStore = JSON.parse( localStorage.getItem('cacheStore')! )
+}
+
+  private getCountryRequest(url: string):Observable<Country[]> {
+    return this.http.get<Country[]>( url )
+      .pipe(
+        catchError( error => of([]) ),
+        // delay(2000),
+      )
+  }
 
   searchCountrybyAlphaCode ( code: string): Observable<Country | null > {
     const url =`${ this.apiUrl }/alpha/${ code }`;
@@ -22,26 +51,29 @@ export class CountriesService {
 
   searchCapital(term: string ): Observable<Country[]> {
     const url = `${ this.apiUrl }/capital/${ term }`;
-
-    return this.getCountryByUrl(url)
+    return this.getCountryRequest(url)
+      .pipe(
+        tap( countries => this.cacheStore.byCapital = { term, countries } ),
+        tap( () => this.saveToLocalStorage() ),
+      )
   }
 
   searchCountry(term: string): Observable<Country[]>{
     const url =`${ this.apiUrl }/name/${ term }`;
-
-    return this.getCountryByUrl(url)
-  }
-
-  searchRegion(term: string): Observable<Country[]>{
-    const url =`${ this.apiUrl }/region/${ term }`;
-
-    return this.getCountryByUrl(url)
-  }
-
-  public getCountryByUrl(url: string):Observable<Country[]> {
-    return this.http.get<Country[]>( url )
+    return this.getCountryRequest(url)
       .pipe(
-        catchError( error => of([]) )
+        tap( countries => this.cacheStore.byCountries = { term, countries }),
+        tap( () => this.saveToLocalStorage() ),
       )
   }
+
+  searchRegion(region: Region): Observable<Country[]>{
+    const url =`${ this.apiUrl }/region/${ region }`;
+    return this.getCountryRequest(url)
+      .pipe(
+        tap( countries => this.cacheStore.byRegion= { region , countries } ),
+        tap( () => this.saveToLocalStorage() ),
+      )
+  }
+
 }
--- src/app/shared/components/search-box/search-box.component.ts
+++ src/app/shared/components/search-box/search-box.component.ts
@@ -1,4 +1,5 @@
-import { Component,Input,Output,EventEmitter } from '@angular/core';
+import { Component,Input,Output,EventEmitter, OnInit,OnDestroy } from '@angular/core';
+import { Subject, Subscription, debounceTime } from 'rxjs';
 
 @Component({
   selector: 'shared-search-box',
@@ -6,17 +7,43 @@ import { Component,Input,Output,EventEmitter } from '@angular/core';
   styles: [
   ]
 })
-export class SearchBoxComponent {
+export class SearchBoxComponent implements OnInit, OnDestroy {
+
+  private debouncer: Subject<string> = new Subject<string>();
+  private deboucerSubscription?: Subscription;
 
   @Output()
   public onValue = new EventEmitter<string>();
 
+  @Output()
+  public onDebounce = new EventEmitter<string>();
+
   @Input()
   public placeholder:string = '';
+
+  @Input()
+  public initialValue:string = '';
+
+  ngOnInit(): void {
+    this.deboucerSubscription= this.debouncer
+    .pipe(
+      debounceTime(300)
+      )
+      .subscribe( value =>{
+        this.onDebounce.emit( value );
+      })
+    }
+
+    ngOnDestroy(): void {
+      this.deboucerSubscription?.unsubscribe();
+    }
 
   emitValue( value:string ):void{
     this.onValue.emit( value )
   }
 
+  onKeyPress( serchTerm: string ){
+    this.debouncer.next( serchTerm )
+  }
 
 }
